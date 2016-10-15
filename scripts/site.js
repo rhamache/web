0f7388bb68f3601899aef53a03e6b0fd84a281de
Ryan Hamacher <Ryan Hamacher>
2016-10-15 12:00:43 -0600

    Adding scroll effect for links. Adding fixed navbar. Adding addtional parallax panel with new background

---
+++ scripts/site.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+
+    // scroll effect for anchors
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            var nav = $("nav")
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - nav.height()
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
